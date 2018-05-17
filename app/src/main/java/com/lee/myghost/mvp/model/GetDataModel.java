4b122a6f6a07646426a45c0fc1b69958406a0917
LizhiT <714853350@qq.com>
2018-05-17 14:01:02 +0800

    修改model层的数据

--- app/src/main/java/com/lee/myghost/mvp/model/GetDataModel.java
+++ app/src/main/java/com/lee/myghost/mvp/model/GetDataModel.java
@@ -1,33 +1,67 @@
 package com.lee.myghost.mvp.model;
 
+import android.util.Log;
+
 import com.lee.myghost.mvp.model.contract.modelinter.BaseModel;
 import com.lee.myghost.mvp.model.contract.presenterinter.PresenterInter;
 import com.lee.myghost.mvp.presenter.GetDataPresenter;
+import com.lee.myghost.utils.RetrofitUtil;
 
 import java.util.Map;
 
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
 
 /**
  * @author Lee
  * @create_time 2018/5/16 20:01
- * @description
+ * @description 请求网络的Model层
  */
 public class GetDataModel implements BaseModel {
     private PresenterInter presenterInter;
     private Disposable     d;
 
     public GetDataModel(GetDataPresenter presenter) {
-
+        this.presenterInter = presenterInter;
+        this.d = d;
     }
 
     @Override
-    public void getDataFromNet(String url, Map<String, String> map) {
+    public void getDataFromNet(String url, final Map<String, String> map) {
+        RetrofitUtil.getService()
+                .doPost(url, map)
+                .subscribeOn(Schedulers.io())//在io线程获取数据
+                .observeOn(AndroidSchedulers.mainThread())//在android主线程梳理数据
+                .subscribe(new Observer<ResponseBody>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        GetDataModel.this.d = d;
+                    }
 
+                    @Override
+                    public void onNext(ResponseBody responseBody) {
+                        //代表获取到数据
+                        presenterInter.onSuccess(responseBody);
+                        Log.d("wangzx", "onNext: " + map.toString());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        //发生错误
+                        presenterInter.onError(e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                    }
+                });
     }
 
     @Override
     public void unsubcribe() {
-
+        d.dispose();
     }
 }
