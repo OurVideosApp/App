ea47c8fc19b723b64488a030168deecb794a3c80
LizhiT <714853350@qq.com>
2018-05-21 21:23:26 +0800

    修改部分功能

---
+++ app/src/main/java/com/lee/myghost/utils/fangtantan_Animation/BaseFlingAdapterView.java
@@ -0,0 +1,40 @@
+package com.lee.myghost.utils.fangtantan_Animation;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.AdapterView;
+/*
+* 发现页面 防探探卡片动画的 Utils
+* 2018-05-21 15:13
+* 王彦勇
+*
+* */
+abstract class BaseFlingAdapterView extends AdapterView {
+    private int heightMeasureSpec;
+    private int widthMeasureSpec;
+    public BaseFlingAdapterView(Context context) {
+        super(context);
+    }
+    public BaseFlingAdapterView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public BaseFlingAdapterView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    @Override
+    public void setSelection(int i) {
+        throw new UnsupportedOperationException("Not supported");
+    }
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        this.widthMeasureSpec = widthMeasureSpec;
+        this.heightMeasureSpec = heightMeasureSpec;
+    }
+    public int getWidthMeasureSpec() {
+        return widthMeasureSpec;
+    }
+    public int getHeightMeasureSpec() {
+        return heightMeasureSpec;
+    }
+}
