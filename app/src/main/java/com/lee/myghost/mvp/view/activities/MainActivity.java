faabd0b62a664e83ac9a56803f534aebd411ccb1
wangzi0717561 <1600377085@qq>
2018-05-23 10:10:48 +0800

    CELAI

--- app/src/main/java/com/lee/myghost/mvp/view/activities/MainActivity.java
+++ app/src/main/java/com/lee/myghost/mvp/view/activities/MainActivity.java
@@ -1,13 +1,19 @@
 package com.lee.myghost.mvp.view.activities;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.lee.myghost.R;
 import com.lee.myghost.mvp.model.base.BaseAvtivity;
@@ -20,20 +26,22 @@ import com.lee.myghost.mvp.view.fragments.SpeicalFragment;
 
 import okhttp3.ResponseBody;
 
-public class MainActivity extends BaseAvtivity<GetDataPresenter> implements GetDataFromNetInter, RadioGroup.OnCheckedChangeListener {
+public class MainActivity extends BaseAvtivity<GetDataPresenter> implements GetDataFromNetInter, RadioGroup.OnCheckedChangeListener, View.OnClickListener {
 
-    private FrameLayout         main_framlayout;
-    private RadioGroup          main_radiogroup;
-    private RadioButton         check_sift;
-    private RadioButton         check_special;
-    private RadioButton         check_find;
-    private RadioButton         check_my;
-    private FragmentManager     manager;
+    private FrameLayout main_framlayout;
+    private RadioGroup main_radiogroup;
+    private RadioButton check_sift;
+    private RadioButton check_special;
+    private RadioButton check_find;
+    private RadioButton check_my;
+    private FragmentManager manager;
     private FragmentTransaction transaction;
-    private ChoicenessFragment  mChoicenessFragment;
-    private SpeicalFragment     mSpeicalFragment;
-    private MineFragment        mMineFragment;
+    private ChoicenessFragment mChoicenessFragment;
+    private SpeicalFragment mSpeicalFragment;
+    private MineFragment mMineFragment;
     private FindFragment mFindFragment;
+    private RelativeLayout my_download, my_collect, my_welfare, my_share, my_suggest, slide_about, slide_theme;
+    private Intent intent;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,6 +60,20 @@ public class MainActivity extends BaseAvtivity<GetDataPresenter> implements GetD
         manager = getSupportFragmentManager();
         mChoicenessFragment = new ChoicenessFragment();
         main_radiogroup.setOnCheckedChangeListener(this);
+        my_collect = findViewById(R.id.my_collect);
+        my_download = findViewById(R.id.my_download);
+        my_welfare = findViewById(R.id.my_welfare);
+        my_share = findViewById(R.id.my_share);
+        my_suggest = findViewById(R.id.my_suggest);
+        slide_about = findViewById(R.id.slide_about);
+        slide_theme = findViewById(R.id.slide_theme);
+        my_collect.setOnClickListener(this);
+        my_download.setOnClickListener(this);
+        my_welfare.setOnClickListener(this);
+        my_share.setOnClickListener(this);
+        my_suggest.setOnClickListener(this);
+        slide_about.setOnClickListener(this);
+        slide_theme.setOnClickListener(this);
     }
 
     @Override
@@ -148,5 +170,33 @@ public class MainActivity extends BaseAvtivity<GetDataPresenter> implements GetD
         if (mMineFragment != null) {
             fragment.hide(mMineFragment);
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.my_collect:
+                intent = new Intent(MainActivity.this, CollectActivity.class);
+                break;
+            case R.id.my_download:
+                Toast.makeText(MainActivity.this,"待开发",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.my_welfare:
+                intent = new Intent(MainActivity.this, WelfareActivity.class);
+                break;
+            case R.id.my_share:
+                intent = new Intent(MainActivity.this, ShareActivity.class);
+                break;
+            case R.id.my_suggest:
+                intent = new Intent(MainActivity.this, SuggestActivity.class);
+                break;
+            case R.id.slide_about:
+                intent = new Intent(MainActivity.this, AboutActivity.class);
+                break;
+            case R.id.slide_theme:
+                intent = new Intent(MainActivity.this, ThemeActivity.class);
+                break;
+        }
+        startActivity(intent);
     }
 }
