78d9166c11c202936396915e987c7bf743dbea7e
LizhiT <714853350@qq.com>
2018-05-24 14:28:54 +0800

    添加弹出框

--- app/src/main/java/com/lee/myghost/mvp/view/activities/MainActivity.java
+++ app/src/main/java/com/lee/myghost/mvp/view/activities/MainActivity.java
@@ -1,52 +1,86 @@
 package com.lee.myghost.mvp.view.activities;
 
-import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lee.myghost.R;
 import com.lee.myghost.mvp.model.base.BaseAvtivity;
 import com.lee.myghost.mvp.model.contract.viewinter.GetDataFromNetInter;
 import com.lee.myghost.mvp.presenter.GetDataPresenter;
+import com.lee.myghost.mvp.view.fragments.ChoicenessFragment;
 import com.lee.myghost.mvp.view.fragments.FindFragment;
 import com.lee.myghost.mvp.view.fragments.MineFragment;
-import com.lee.myghost.mvp.view.fragments.ChoicenessFragment;
 import com.lee.myghost.mvp.view.fragments.SpeicalFragment;
+import com.lee.myghost.utils.ShareUtil;
 
 import okhttp3.ResponseBody;
 
 public class MainActivity extends BaseAvtivity<GetDataPresenter> implements GetDataFromNetInter, RadioGroup.OnCheckedChangeListener, View.OnClickListener {
 
-    private FrameLayout main_framlayout;
-    private RadioGroup main_radiogroup;
-    private RadioButton check_sift;
-    private RadioButton check_special;
-    private RadioButton check_find;
-    private RadioButton check_my;
-    private FragmentManager manager;
+    String ANDROID_ID = android.os.Build.MODEL;
+    private FrameLayout         main_framlayout;
+    private RadioGroup          main_radiogroup;
+    private RadioButton         check_sift;
+    private RadioButton         check_special;
+    private RadioButton         check_find;
+    private RadioButton         check_my;
+    private FragmentManager     manager;
     private FragmentTransaction transaction;
-    private ChoicenessFragment mChoicenessFragment;
-    private SpeicalFragment mSpeicalFragment;
-    private MineFragment mMineFragment;
-    private FindFragment mFindFragment;
-    private RelativeLayout my_download, my_collect, my_welfare, my_share, my_suggest, slide_about, slide_theme;
-    private Intent intent;
+    private ChoicenessFragment  mChoicenessFragment;
+    private SpeicalFragment     mSpeicalFragment;
+    private MineFragment        mMineFragment;
+    private FindFragment        mFindFragment;
+    private RelativeLayout      my_download, my_collect, my_welfare, my_share, my_suggest, slide_about, slide_theme;
+    private Intent   intent;
+    /**
+     * 反馈
+     */
+    private TextView mSuggestTitle;
+    /**
+     * 请输入您的邮箱(必填)
+     */
+    private EditText mSuggestEmail;
+    private View     mSuggestView;
+    /**
+     * 请输入您的反馈...
+     */
+    private EditText mSuggestFeedback;
+    /**
+     * 按住录音
+     */
+    private Button   mHoldTheTape;
+    /**
+     * 上传屏幕截图
+     */
+    private CheckBox mUploadingScreenshot;
+    /**
+     * 设备详情
+     */
+    private TextView mGetPhoneInfo;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("TAG", "setChildContentView: " + "执行onCreate");
+        initView();
     }
 
     @Override
@@ -74,6 +108,7 @@ public class MainActivity extends BaseAvtivity<GetDataPresenter> implements GetD
         my_suggest.setOnClickListener(this);
         slide_about.setOnClickListener(this);
         slide_theme.setOnClickListener(this);
+
     }
 
     @Override
@@ -180,19 +215,17 @@ public class MainActivity extends BaseAvtivity<GetDataPresenter> implements GetD
                 startActivity(intent);
                 break;
             case R.id.my_download:
-                Toast.makeText(MainActivity.this,"待开发",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "待开发", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.my_welfare:
                 intent = new Intent(MainActivity.this, WelfareActivity.class);
                 startActivity(intent);
                 break;
             case R.id.my_share:
-                intent = new Intent(MainActivity.this, ShareActivity.class);
-                startActivity(intent);
+                ShareUtil.shareText(this, "这是我的App", "分享到");
                 break;
             case R.id.my_suggest:
-                intent = new Intent(MainActivity.this, SuggestActivity.class);
-                startActivity(intent);
+                popWindowMethod();
                 break;
             case R.id.slide_about:
                 intent = new Intent(MainActivity.this, AboutActivity.class);
@@ -202,7 +235,51 @@ public class MainActivity extends BaseAvtivity<GetDataPresenter> implements GetD
                 intent = new Intent(MainActivity.this, ThemeActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.hold_the_tape:
+                break;
         }
 
     }
+
+    public void popWindowMethod() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        View view = View.inflate(this, R.layout.suggest_dialog_item, null);
+        mSuggestTitle = (TextView) view.findViewById(R.id.suggest_title);
+        mSuggestEmail = (EditText) view.findViewById(R.id.suggest_email);
+        mSuggestView = (View) view.findViewById(R.id.suggest_view);
+        mSuggestFeedback = (EditText) view.findViewById(R.id.suggest_feedback);
+        mHoldTheTape = (Button) view.findViewById(R.id.hold_the_tape);
+        mHoldTheTape.setOnClickListener(this);
+        mUploadingScreenshot = (CheckBox) view.findViewById(R.id.uploading_screenshot);
+        mGetPhoneInfo = (TextView) view.findViewById(R.id.get_phone_info);
+        mGetPhoneInfo.setText("设备型号：  " + getSystemModel() + "  " + getSystemVersion());
+        alertDialogBuilder.setView(view).setPositiveButton("取消", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).setNegativeButton("发送", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).show();
+    }
+
+    /**
+     * 获取手机型号
+     *
+     * @return 手机型号
+     */
+    public static String getSystemModel() {
+        return android.os.Build.MODEL;
+    }
+
+    /**
+     * 获取当前手机系统版本号
+     *
+     * @return 系统版本号
+     */
+    public static String getSystemVersion() {
+        return android.os.Build.VERSION.RELEASE;
+    }
+
 }
